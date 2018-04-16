a49669217502e6990898e4d75ee3f5c3b5f30254
Matthew OLDACH <matthewoldach@Matthews-MacBook.local>
2018-04-16 09:51:37 -0700

    hangman work

---
+++ game.js
@@ -0,0 +1,8 @@
+// word selection is random
+
+exports.game = {
+
+    
+        wordList:['RAIDERS','STEELERS','VIKINGS','CARDINALS','BEARS','GIANTS','PACKERS','LIONS','REDSKINS','EAGLES','RAMS','CHARGERS','BRONCOS', 'PATRIOTS', 'BROWNS', 'COLTS', 'CHIEFS', 'COWBOYS'],
+        hangman: ["\n\n\n\n\n\n\n-------------", "\n |\n |\n |\n |\n |\n |\n |\n-------------", "____________\n |         |\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        |\n |\n-------------","____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        | |\n |\n-------------"]
+      };
